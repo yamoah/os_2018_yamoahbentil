f7313f05ff7d287f1683a1f3b184c409bd9a3044
yamoahbentil <yamoahbentil@gmail.com>
2018-09-12 21:39:52 +0000

    file reading and printing completed

--- lab01/my_cat.c
+++ lab01/my_cat.c
@@ -1,21 +1,29 @@
 /*yamoah bentil   my_cat */
-
 #include <stdio.h>
 /*
 int main(void){
 	printf("Hello");
 	printf("\n");
+	int c;
+	while((c=getchar())!=EOF)
+		putchar(c);
 }
-
 int main(int argc, char *argv[]){
 	int i;
 	for(i=1; i<argc; ++i)
 		printf("%s", argv[i]);
 	printf("\n");
 }
-
-int main(void){
-	int c;
-	while((c=getchar())!=EOF)
-		putchar(c);
-}*/
+*/
+int main(){
+	char c;
+	FILE *infile;
+	infile = fopen("testdoc.txt","r");
+	c = fgetc(infile);
+	while (c !=EOF){
+		printf("%c", c);
+		c = fgetc(infile);
+	}
+	fclose(infile);
+	return 0;
+}
