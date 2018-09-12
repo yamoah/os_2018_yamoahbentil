8c97e0e875e93df03cc0136c48b34585b0dab297
yamoahbentil <yamoahbentil@gmail.com>
2018-09-12 21:14:35 +0000

    read user input (But commented out)

---
+++ lab01/my_cat.c
@@ -0,0 +1,21 @@
+/*yamoah bentil   my_cat */
+
+#include <stdio.h>
+/*
+int main(void){
+	printf("Hello");
+	printf("\n");
+}
+
+int main(int argc, char *argv[]){
+	int i;
+	for(i=1; i<argc; ++i)
+		printf("%s", argv[i]);
+	printf("\n");
+}
+
+int main(void){
+	int c;
+	while((c=getchar())!=EOF)
+		putchar(c);
+}*/
