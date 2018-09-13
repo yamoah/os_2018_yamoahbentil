80747d1ee762d4a1500d5ec2703078a650fd0b08
yamoahbentil <yamoahbentil@gmail.com>
2018-09-13 06:56:23 +0000

    semi-finished version of greg but not compiling

--- lab01/my_greg.c
+++ lab01/my_greg.c
@@ -1,4 +1,3 @@
-
 /*yamoah bentil   my_greg */
 #include <stdio.h>
 #include <string.h>
@@ -8,15 +7,15 @@ int findPattern(char pattern[], char text[]);
 int main(int argc, char *argv[]){
 	char c;
 	FILE *infile;
-	infile = fopen(argv[1],"r");
+	char *patt = argv[1];
+	infile = fopen(argv[2],"r");
 	c = fgetc(infile);
 	if (infile == NULL){
 		printf("Cannot open file \n"); 
-		exit(0); 
+		return 1;/*exit(0);*/
     }
 	while (c !=EOF){
-		/*printf("%c", c);*/
-		
+		findPattern(patt,c);
 		c = fgetc(infile);
 	}
 	fclose(infile);
@@ -24,5 +23,21 @@ int main(int argc, char *argv[]){
 }
 
 int findPattern(char pattern[], char text[]){
+	int i, j, count, lenPattern, lenText =0;
+	lenPattern = strlen(pattern);
+	lenText = strlen(text);
+	if(lenPattern > lenText)
+		return 0;
+	else{
+		for(i=0; i<=lenText-lenPattern; i++){
+			count = 0;
+			for(j=0; j<lenPattern; j++){
+				if(pattern[j]=text[j+i])
+					count++;
+			}
+			if(count==lenPattern)
+				printf(text);
+		}
+	}
 	return 0;
 }
