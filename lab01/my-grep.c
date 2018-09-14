26e9590a4bf6a35f6ebca153f00e4ff4e0079515
yamoah <yamoahbentil@gmail.com>
2018-09-14 16:32:21 +0000

    replaced get character with get string. Not preprocessing needed before passing line to findPattern(). Code completed. Next: Modify previous code...

--- lab01/my-grep.c
+++ lab01/my-grep.c
@@ -1,33 +1,33 @@
-/*yamoah bentil  my_grep */
+/*yamoah bentil  my-grep_2 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int findPattern(char pattern[], char text[]);
+void helpMessage();
+char myLine[255];
 
 int main(int argc, char *argv[]){
-	char* c;
-	int cc;
-	/*FILE *infile; char cc = patt; */
+	
 	char *patt = argv[1];
+	if (patt==NULL){
+		helpMessage();
+	}
 	
 	FILE *infile = fopen(argv[2],"r");
-	c,cc = fgetc(infile);
 	if (infile == NULL){
-		printf("Cannot open file \n");
-		exit(0);
+		helpMessage();
     }
-	while (cc !=EOF){
-		/*printf("%c", c);*/
-		findPattern(patt, c);
-		c,cc = fgetc(infile);
+    
+    while(fgets(myLine, 255, infile)){
+		findPattern(patt, myLine);
 	}
 	fclose(infile);
 	return 0;
 }
 
 int findPattern(char pattern[], char text[]){
-	int i, j, count, lenPattern, lenText = -1;
+	int i, j, count, lenPattern, lenText = 0;
 	lenPattern = strlen(pattern);
 	lenText = strlen(text);
 	if(lenPattern > lenText)
@@ -36,14 +36,21 @@ int findPattern(char pattern[], char text[]){
 		for(i=0; i<=lenText-lenPattern; i++){
 			count = 0;
 			for(j=0; j<lenPattern; j++){
-				if(pattern[j]=text[j+i])
+				if(pattern[j]==text[j+i])
 					count++;
 			}
 			if(count==lenPattern){
-				printf(text);
 				i=(lenText-lenPattern)+1;
+				printf(text);
 			}	
 		}
 	}
 	return 0;
+}
+
+void helpMessage(){
+	printf("missing parameter(s), quiting.....\n");
+	printf("try 'my-grep' 'pattern' 'filename' \n");
+	printf("eg my-grep foo hello.txt\n");
+	exit(0);
 }
