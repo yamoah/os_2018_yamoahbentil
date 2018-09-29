b7c9f32c8db6d8497caa25521ed1be070f45751e
yamoah <yamoahbentil@gmail.com>
2018-09-29 23:22:09 +0000

    created function to handle on parameter and one parameter instances

--- lab02/wish.c
+++ lab02/wish.c
@@ -2,7 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 15
 
-int main(){
-	printf("Hello");
+void doWish();
+void doBatchWish();
+
+
+int main(int argc, char *argv[]){
+	
+    char *batchfile = argv[1];
+    char *otherCommand = argv[2];
+    if (batchfile==NULL){
+    	doWish();
+	}
+    else{
+    	if (otherCommand==NULL){
+    		doBatchWish();
+		}
+		else{
+			printf("use either one argument or no arguments");
+		}
+	}
+    return 0;
+}
+
+void doWish(){
+	printf("wish> ");
+	
+	char buf[MAX]; 
+    fgets(buf, MAX, stdin);
+    
+    //char* myString;
+    //scanf("%s", &myString);
+    //while(myString!="exit"){
+    while(strcmp(buf,"exit") != 0){
+    	//printf("%s", &myString);
+    	//scanf("%s", &myString);
+		printf("string is: %s", buf);
+		printf("wish> ");
+		
+		if(strcmp(buf,"exit") == 0){
+			printf("match here");
+		}
+		fgets(buf, MAX, stdin);
+	}
+	//printf("the no arg one");
+}
+
+void doBatchWish(){
+	printf("the batch one");
 }
--- lab02/wish2.c
+++ lab02/wish2.c
@@ -2,23 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 15
+
+void doWish();
+void doBatchWish();
+
 
 int main(int argc, char *argv[]){
-	/** int a, b, c;
-    printf("Enter the first value:");
-    scanf("%d", &a);
-    printf("Enter the second value:");
-    scanf("%d", &b);
-    c = a + b;
-    printf("%d + %d = %d\n", a, b, c); **/
-    printf("wish>");
-    char* myString;
-    scanf("%s", &myString);
-    while(myString!="exit"){
-    	printf("wish >");
-    	//printf("%s", &myString);
-    	scanf("%s", &myString);
+	
+    char *batchfile = argv[1];
+    char *otherCommand = argv[2];
+    if (batchfile==NULL){
+    	doWish();
 	}
-    
+    else{
+    	if (otherCommand==NULL){
+    		doBatchWish();
+		}
+		else{
+			printf("use either one argument or no arguments");
+		}
+	}
     return 0;
+}
+
+void doWish(){
+	printf("wish> ");
+	
+	char buf[MAX]; 
+    fgets(buf, MAX, stdin);
+    
+    //char* myString;
+    //scanf("%s", &myString);
+    //while(myString!="exit"){
+    while(buf!="exit"){
+    	//printf("%s", &myString);
+    	//scanf("%s", &myString);
+		printf("string is: %s", buf);
+		printf("wish> ");
+		fgets(buf, MAX, stdin);
+		if(buf=="exit"){
+			printf("A match");
+		}
+		
+	}
+	//printf("the no arg one");
+}
+
+void doBatchWish(){
+	printf("the batch one");
 }
